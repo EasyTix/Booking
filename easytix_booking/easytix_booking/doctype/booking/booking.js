670d52c7ff06f854f2436c9e416c292d975d63ab
brian <brian@netquestsolutions.com>
2025-06-10 12:19:53 +0800

    Limit Passenger Grid

--- easytix_booking/easytix_booking/doctype/booking/booking.js
+++ easytix_booking/easytix_booking/doctype/booking/booking.js
@@ -13,10 +13,13 @@ frappe.ui.form.on("Booking", {
 	},
 	refresh: function (frm) {
 		frm.fields_dict["participants"].grid.no_customize = true;
+
 		// Prevent selecting a past date when creating a booking
-		frm.fields_dict.booking_date.datepicker.update({
-			minDate: new Date(frappe.datetime.get_today()),
-		});
+		if (frm.is_new()) {
+			frm.fields_dict.booking_date.datepicker.update({
+				minDate: new Date(frappe.datetime.get_today()),
+			});
+		}
 
 		//Approve or Reject Booking
 		if (
@@ -50,5 +53,29 @@ frappe.ui.form.on("Booking", {
 
 		// Refresh the Participants table
 		frm.refresh_field("participants");
+	},
+});
+
+frappe.ui.form.on("Booking Participant", {
+	participants_add: function (frm, cdt, cdn) {
+		// Get the current number of rows in the participants table
+		let current_rows = frm.doc.participants ? frm.doc.participants.length : 0;
+		// Get the quantity value
+		let quantity = frm.doc.quantity || 0;
+
+		// Check if adding the new row exceeds the quantity
+		if (current_rows > quantity) {
+			// Remove the newly added row
+			frm.get_field("participants").grid.grid_rows_by_docname[cdn].remove();
+			frm.refresh_field("participants");
+			frappe.msgprint({
+				title: __("Warning"),
+				indicator: "orange",
+				message: __(
+					"Cannot add more participants. The limit is {0} based on the specified quantity.",
+					[quantity]
+				),
+			});
+		}
 	},
 });
